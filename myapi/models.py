19962630bc92692bc67e6c9107eba80b114508d5
Alec Wells <alec.wells47@gmail.com>
2019-12-17 15:08:53 -0700

    Fix Restaurant model to include all necessary fields
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- myapi/models.py
+++ myapi/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Hero(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=400)
     alias = models.CharField(max_length=60)
 
     def __str__(self):
         return self.name
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=60)
-    yelp_id = models.CharField(max_length=60)
-    phone = models.CharField(max_length=60)
-    is_closed = models.BooleanField(default=True)
-    review_count = models.IntegerField()
-    url = models.CharField(max_length=60)
-    latitude = models.FloatField(max_length=100)
-    longitude = models.FloatField(max_length=100)
-    image_url = models.CharField(max_length=60)
-    address = models.CharField(max_length=60)
-    distance = models.FloatField(max_length=100)
+    name = models.CharField(max_length=400)
+    yelp_id = models.CharField(max_length=400)
+    phone = models.CharField(max_length=400, null=True)
+    is_closed = models.BooleanField(null=True)
+    review_count = models.IntegerField(default=0)
+    url = models.CharField(max_length=400, null=True)
+    latitude = models.FloatField(max_length=100, null=True)
+    longitude = models.FloatField(max_length=100, null=True)
+    image_url = models.CharField(max_length=400, null=True)
+    address = models.CharField(max_length=100, null=True)
+    distance = models.FloatField(max_length=100, null=True)
     # transactions = ArrayField(models.CharField(max_length=50), blank=True)
     tacoboutit_item_review_count = models.IntegerField(default=0)
 
