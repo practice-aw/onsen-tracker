b4445d22c33cf5ed72844c5eb97783bf6e85f576
Alec Wells <alec.wells47@gmail.com>
2019-12-26 11:13:18 -0600

    Register taco with admin site
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Hero
-from .models import Restaurant
+from .models import Restaurant, Taco
 
 # Register your models here.
 admin.site.register(Hero)
 admin.site.register(Restaurant)
+admin.site.register(Taco)
