f802f39420d8104d4a6d0d6cc73ccaa97ba1b5fb
Alec Wells <alec.wells47@gmail.com>
2019-12-27 12:56:07 -0600

    Define taco serializer in restaurant serializer and add field
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -15,8 +15,8 @@ class TacoSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
-    # consider adding this tacos = TacoSerializer(many=True, read_only=True)
+    tacos = TacoSerializer(many=True, read_only=True)
     class Meta:
         model = Restaurant
-        fields = ('id', 'yelp_id', 'name', 'phone', 'is_closed', 'review_count', 'yelp_rating', 'url', 'latitude', 'longitude', 'image_url', 'address', 'distance', 'tacoboutit_item_review_count')
+        fields = ('id', 'yelp_id', 'name', 'phone', 'is_closed', 'review_count', 'yelp_rating', 'url', 'latitude', 'longitude', 'image_url', 'address', 'distance', 'tacoboutit_item_review_count', 'tacos')
         # add tacos to fields
