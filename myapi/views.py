ce31b355c4de41403907128b777322f13a24170a
Alec Wells <alec.wells47@gmail.com>
2020-01-03 14:59:51 -0700

    Fix merge conflict in views for repeat methods
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- myapi/views.py
+++ myapi/views.py
@@ -75,8 +75,29 @@ class RestaurantUpdateOrCreateViewSet(viewsets.ModelViewSet):
         all_data_dicts = []
         yelp_ids = []
         if(business_data['total'] != 0):
+<<<<<<< Updated upstream
             data_org(business_data, all_data_dicts, yelp_ids)
 
+=======
+            data_org(business_data, all_data_dicts, yelp_ids, data_dict)
+            # for data in business_data['businesses']:
+            #     # data_dict = {}
+            #     data_dict['yelp_id'] = data['id']
+            #     data_dict['name'] = data['name']
+            #     data_dict['phone'] = data['phone']
+            #     data_dict['is_closed'] = data['is_closed']
+            #     data_dict['review_count'] = data['review_count']
+            #     data_dict['yelp_rating'] = data['rating']
+            #     data_dict['url'] = data['url']
+            #     data_dict['latitude'] = data['coordinates']['latitude']
+            #     data_dict['longitude'] = data['coordinates']['longitude']
+            #     data_dict['image_url'] = data['image_url']
+            #     a = ", "
+            #     data_dict['address'] = a.join(data['location']['display_address'])
+            #     data_dict['distance'] = data['distance']
+            #     all_data_dicts.append(data_dict)
+            #     yelp_ids.append(data['id'])
+>>>>>>> Stashed changes
             for data in all_data_dicts:
                 print("this is where you are tru", data['yelp_id'])
                 Restaurant.objects.update_or_create(
