d18c8958291a1e43a41af4b532371d8d8cbd3c8a
scott <spayton.c@gmail.com>
2020-01-05 14:25:10 -0700

    remove STR test

--- tacoboutit/tests/test_models.py
+++ tacoboutit/tests/test_models.py
@@ -7,7 +7,7 @@ class TestModels:
         #create the taco
         rest = mixer.blend('myapi.Restaurant', name='scotts tacos')
         assert rest.tacos
-
-    def test_str_method(self):
-        rest = mixer.blend('myapi.Restaurant', name='scotts tacos')
-        assert rest.__str_()
+        
+    # def test_str_method(self):
+    #     rest = mixer.blend('myapi.Restaurant', name='scotts tacos')
+    #     assert rest.__str_()
