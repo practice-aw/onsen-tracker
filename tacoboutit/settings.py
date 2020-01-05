70fa85d5513f323fcb52555756161eea24709882
Alec Wells <alec.wells47@gmail.com>
2020-01-05 14:31:39 -0700

    Add intreped to installed_apps
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- tacoboutit/settings.py
+++ tacoboutit/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'intreped'
     'myapi',
     'rest_framework',
     'polls',
