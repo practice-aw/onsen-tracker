1b10a4f5ee4d9190201c94f60845e6a4f1542716
Alec Wells <alec.wells47@gmail.com>
2020-01-05 14:43:39 -0700

    Fix typo in settings and travis.yml
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- tacoboutit/settings.py
+++ tacoboutit/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'intreped'
     'myapi',
     'rest_framework',
     'polls',
