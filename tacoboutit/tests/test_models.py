94d0d14c6f113dc1009582af17ba12aa0bf89c5d
Alec Wells <alec.wells47@gmail.com>
2020-01-05 14:48:56 -0700

    Add preliminary tests for travis ci
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

---
+++ tacoboutit/tests/test_models.py
@@ -0,0 +1,13 @@
+from mixer.backend.django import mixer
+import pytest
+@pytest.mark.django_db
+class TestModels:
+
+    def test_restaurant_has_tacos(self):
+        #create the taco
+        rest = mixer.blend('myapi.Restaurant', name='scotts tacos')
+        assert rest.tacos
+
+    # def test_str_method(self):
+    #     rest = mixer.blend('myapi.Restaurant', name='scotts tacos')
+    #     assert rest.__str_()
