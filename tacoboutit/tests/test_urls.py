1b27fb3a2ddfb5ad3bc4f173f8e23bee6d51724c
scott <spayton.c@gmail.com>
2020-01-06 15:02:21 -0700

    Create working test for restaurants retrieve endpoint
    Co-Authored-By: Alec Wells <alec.wells47@gmail.com>

--- tacoboutit/tests/test_urls.py
+++ tacoboutit/tests/test_urls.py
@@ -1,6 +1,32 @@
-from django.urls import reverse, resolve
+import json
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from decouple import config
+from django.utils.html import escape
 
-class TestUrls:
-    def test_newtaco_url(self):
-        path = reverse('new_taco')
-        assert resolve(path).view_name == 'new_taco'
+
+
+class ApiTestCase(APITestCase):
+    def test_call(self):
+        response = self.client.get('/api/v1/restaurants/retrieve/', {'lat': 30, 'lng': -104})
+        data ={
+            "id": 1,
+            "yelp_id": "DmpNJIjh1MtlnN36qw_9dw",
+            "name": "Marfa Burrito",
+            "phone": "",
+            "is_closed": False,
+            "review_count": 137,
+            "yelp_rating": 4,
+            "url": "https://www.yelp.com/biz/marfa-burrito-marfa-2?adjust_creative=pxQ3XEqH9sM15FQYWsuBXQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=pxQ3XEqH9sM15FQYWsuBXQ",
+            "latitude": 30.3064881396931,
+            "longitude": -104.019323420231,
+            "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/-6-hdZM4A9i3SNArPp_bnw/o.jpg",
+            "address": "S Highland Ave, Marfa, TX 79843",
+            "distance": 34130.4942084879,
+            "tacoboutit_item_review_count": 0,
+            "tacos": []
+        }
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(content[0],data)
