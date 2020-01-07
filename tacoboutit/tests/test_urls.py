e6973e07fbf5abfb0d479fc737585f84216955de
Alec Wells <alec.wells47@gmail.com>
2020-01-07 16:31:05 -0700

    Finish test for get /api/v1/restaurants/retrieve
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- tacoboutit/tests/test_urls.py
+++ tacoboutit/tests/test_urls.py
@@ -15,7 +15,7 @@ class ApiTestCase(APITestCase):
     def test_restaurants_retrieve(self):
         response = self.client.get('/api/v1/restaurants/retrieve/', {'lat': 30, 'lng': -104})
         data ={
-            "id": 1,
+            "id": 2,
             "yelp_id": "DmpNJIjh1MtlnN36qw_9dw",
             "name": "Marfa Burrito",
             "phone": "",
@@ -27,15 +27,28 @@ class ApiTestCase(APITestCase):
             "longitude": -104.019323420231,
             "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/-6-hdZM4A9i3SNArPp_bnw/o.jpg",
             "address": "S Highland Ave, Marfa, TX 79843",
-            "distance": 34130.4942084879,
+            "distance": 34130.494208487944,
             "tacoboutit_item_review_count": 0,
             "tacos": []
         }
         content = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(content[0],data)
-
-    def test
+        # print(content[0]['id'])
+        self.assertEqual(content[0]['id'], data['id'])
+        self.assertEqual(content[0]['yelp_id'], data['yelp_id'])
+        self.assertEqual(content[0]['name'], data['name'])
+        self.assertEqual(content[0]['phone'], data['phone'])
+        self.assertEqual(content[0]['is_closed'], data['is_closed'])
+        self.assertEqual(content[0]['review_count'], data['review_count'])
+        self.assertEqual(content[0]['yelp_rating'], data['yelp_rating'])
+        self.assertEqual(content[0]['url'], data['url'])
+        self.assertEqual(content[0]['latitude'], data['latitude'])
+        self.assertEqual(content[0]['longitude'], data['longitude'])
+        self.assertEqual(content[0]['image_url'], data['image_url'])
+        self.assertEqual(content[0]['address'], data['address'])
+        self.assertEqual(content[0]['distance'], data['distance'])
+        self.assertEqual(content[0]['tacoboutit_item_review_count'], data['tacoboutit_item_review_count'])
+        self.assertEqual(content[0]['tacos'], data['tacos'])
 
 # class RestaurantModel:
 #class ReviewModel:
