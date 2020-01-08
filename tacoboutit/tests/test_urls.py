1a2dcf10470d90b9fe48a320e97036a530f86adb
Alec Wells <alec.wells47@gmail.com>
2020-01-07 19:58:24 -0700

    Finish testing for reviews
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- tacoboutit/tests/test_urls.py
+++ tacoboutit/tests/test_urls.py
@@ -5,7 +5,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from decouple import config
 from django.utils.html import escape
-from myapi.models import Restaurant, Taco
+from myapi.models import Restaurant, Taco, Review
 
 #rest and taco show pages /:id
 #tacos/new/
@@ -114,3 +114,81 @@ class RestaurantTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(content['status'], 'Resource Not Found')
+
+class ReviewTestCase(APITestCase):
+    def setUp(self):
+        restuarant = Restaurant.objects.create(name='scotts tacos',
+                        yelp_id = "akbkhadfb",
+                        phone = "123456789",
+                        is_closed = False,
+                        review_count = 10,
+                        yelp_rating = 4.7,
+                        url = "http.hfhsf",
+                        latitude = 47.60934,
+                        longitude = -122.34076,
+                        image_url = "https://s3-media3.fl.yelpcdn.com/bphoto/LFo_pxrIEmbVhkKSzZ0jiA/o.jpg",
+                        address = "1521 1st Ave, Seattle, WA 98101",
+                        distance = 100.7,
+                        tacoboutit_item_review_count = 0)
+        # print(restuarant.id)
+        taco = Taco.objects.create(type='al pastor test taco', restaurant_id=restuarant.id)
+        review = Review.objects.create(rating=7, review='terrible', taco_id=taco.id)
+
+    def test_get_all_reviews(self):
+        taco_id = Taco.objects.get(type='al pastor test taco').id
+        review_id = Review.objects.get(review='terrible').id
+        response = self.client.get('/api/v1/reviews/')
+
+        self.assertEqual(Review.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['id'], review_id)
+        self.assertEqual(response.data[0]['rating'], 7)
+        self.assertEqual(response.data[0]['taco'], taco_id)
+
+    def test_get_one_review(self):
+        taco_id = Taco.objects.get(type='al pastor test taco').id
+        review_id = Review.objects.get(review='terrible').id
+        response = self.client.get(f'/api/v1/reviews/{review_id}/')
+
+        self.assertEqual(Review.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], review_id)
+        self.assertEqual(response.data['rating'], 7)
+        self.assertEqual(response.data['taco'], taco_id)
+
+    def test_get_one_review_bad_id(self):
+        response = self.client.get('/api/v1/reviews/433334/')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        content = json.loads(response.content)
+
+        self.assertEqual(content['detail'], 'Not found.')
+
+    def test_post_review(self):
+        taco_id = Taco.objects.get(type='al pastor test taco').id
+        response = self.client.post('/api/v1/reviews/', {"rating": 10, "review": "is ok", "taco": taco_id})
+
+        content = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(content['rating'], 10)
+        self.assertEqual(content['review'], 'is ok')
+        self.assertEqual(content['taco'], taco_id)
+
+    def test_post_review_no_taco_id(self):
+        taco_id = Taco.objects.get(type='al pastor test taco').id
+        response = self.client.post('/api/v1/reviews/', {"rating": 10, "review": "is ok"})
+
+        content = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(content['taco'][0], 'This field is required.')
+
+
+    def test_post_review_bad_id(self):
+        response = self.client.post('/api/v1/reviews/', {"rating": 10, "review": "is ok", "taco": 12345})
+
+        content = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(content['taco'][0], 'Invalid pk "12345" - object does not exist.')
