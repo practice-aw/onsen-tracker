7411d920eeede876240974a9b2328c5a13250621
Alec Wells <alec.wells47@gmail.com>
2020-02-14 08:05:17 -0700

    Remove comments from urls

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,15 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-# figure out out to remove trailing slash requirement
+
 router = routers.DefaultRouter()
 router.register(r'tacos', views.TacoViewSet)
 router.register(r'restaurants', views.RestaurantViewSet)
 router.register(r'reviews', views.ReviewViewSet)
 router.register(r'users', views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api/v1/restaurants/retrieve/', views.RestaurantUpdateOrCreateViewSet.retrieve, name='update_or_create'),
     path('api/v1/tacos/new/', views.TacoViewSet.post_new, name='new_taco'),
